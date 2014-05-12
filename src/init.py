6cbf6d4f339932f41a018fff3c49660bae6d1fbb
Jacob Troxel <yacklebeam@gmail.com>
2014-05-12 12:38:21 -0700

    Added help, setup commands to init.py

--- src/init.py
+++ src/init.py
@@ -1,9 +1,41 @@
+import os.path as osp
+
 class MainMenu:
 	"""Main Menu for the CorpusCrawler tool, written for Python 2.7"""
 	def __init__(self):
 		self.is_running = True
+		self.corpus_path = ""
+		self.load_config()
 
-	def display_help(self):
+	def load_config(self):
+		print ("Loading config file 'settings.conf'")
+		config = "../settings.conf"
+		if osp.exists(config): #load config
+			config_file = open(config, "r")
+			for line in config_file:
+				if line[0] != "#":
+					self.load(line)
+		else: #no config, generate one
+			print ("WARNING! Setting file not found. Please update settings.conf before continuing.")
+			new_config = open(config, "w")
+			new_config.write("# Generated settings.conf. Please update before using.\n")
+			new_config.write("# Anything after a '#' will not be read by the program.\n\n")
+			new_config.write("# Where the program will look for a corpus\n")
+			new_config.write("corpus")
+			new_config.close()
+
+	def load(self, line):
+		setting_parts = line.split(" ",1)
+		setting = setting_parts[0]
+		try:
+			value = setting_parts[1]
+		except IndexError:		
+			value = ""
+		if setting == "corpus":
+			print ("...loading corpus path...")
+			self.set_corpus_path(value)
+
+	def display_help(self, flags):
 		print ("HELP MENU")
 
 	def prompt(self):
@@ -21,12 +53,30 @@ class MainMenu:
 		if command == "quit" or command == 'q':
 			self.is_running = False
 			return True
-		return False
+		elif command == "help":
+			self.display_help(flags)
+			return True
+		elif command == "corpus":
+			self.set_corpus_path(flags)
+			return True
+		elif command == "reload":
+			self.load_config()
+			return True
+		else:
+			return False
 
 	def run(self):
-		print ("Welcome to CorpusCrawler v1.0")
 		while self.is_running:
 			self.prompt()
+
+	def set_corpus_path(self, path):
+		if path == "":
+			print ("Corpus path set to current directory.")
+		elif osp.isdir(path):
+			self.corpus_path = path
+			print ("Corpus path set to '" + path + "'.")
+		else:
+			print ("Error: '" + path + "' is not a directory.")
 
 def main():
 	m = MainMenu()
